1934720ebbfad73d3aac9ee6fbfd57f2c6057a64
Ryan Walker <rytwalker@gmail.com>
2019-02-20 11:49:30 -0500

    updated config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
+// EXAMPLE CONFIG
 module.exports = {
-  consumer_key: 'nFYe4RBTDytC5wEwD6PS7YlLp',
-  consumer_secret: 'x9CEhN4ZyZZCDeraFh9jo5ubGmdt1QVAWwlr1ZQKlv09Y4DjA7',
-  access_token: '965386325322010624-C0cBAByFrCABTX87XQtisIjlaSmqW4r',
-  access_token_secret: 'RkVDhsgaKkNlDqL7pXN6AbpFRS7boPhDan3J8Qfi42NLB'
+  consumer_key: 'value',
+  consumer_secret: 'value',
+  access_token: 'value',
+  access_token_secret: 'value'
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-
-}
+};
